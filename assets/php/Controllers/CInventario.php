b8490bb33993689dd1e493a8aec2302c553cf65e
jportegame <jportegame@gmail.com>
2019-01-20 12:29:27 -0500

    CInventario

--- assets/php/Controllers/CInventario.php
+++ assets/php/Controllers/CInventario.php
@@ -4,6 +4,7 @@ if(!isset($include)){
 	include_once '../Conexion.php';
 	include_once 'SesionEmpleado.php';
 	include_once '../Models/Usuario.php';
+	include_once '../Models/Proveedor.php';
 	include_once '../Models/Producto.php';
 	$objectSession =new SesionEmpleado();
 	$method = isset($_GET['method'])?$_GET['method']:"";
