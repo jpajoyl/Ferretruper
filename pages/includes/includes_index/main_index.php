366bb7c4c3bac8192133e80b0802edb44823ad93
jportegame <jportegame@gmail.com>
2019-01-28 10:53:29 -0500

    venta

--- pages/includes/includes_index/main_index.php
+++ pages/includes/includes_index/main_index.php
@@ -2,14 +2,14 @@
 	<div class="card-header">
 		<div class="row">
 			<div class="col-xs-6 col-sm-6 col-md-8 col-lg-10 col-xl-10">
-				<h3><i class="fa fa-table"></i> Inventario actual</h3>
+				<h3><i class="fa fa-table"></i> Realizar venta</h3>
 			</div>
 		</div>
 	</div>											
 	<div class="card-body">
 		<table id="table-productos" class="table table-bordered table-responsive-xl table-hover display">
 			<thead class="cf">
-				<tr>
+				<tr class="tr-head">
 					<th>Id Producto</th>
 					<th>Nombre</th>
 					<th>Ref Fabrica</th>
@@ -23,5 +23,28 @@
 
 			</tbody>
 		</table>
+		<table id="table-venta" class="table table-bordered table-responsive-xl table-hover display">
+			<thead class="cf">
+				<tr class="tr-head">
+					<th>Id Producto</th>
+					<th>Nombre</th>
+					<th>Precio venta</th>
+					<th>Cantidad</th>
+					<th>Total</th>					
+					<th></th>
+				</tr>
+			</thead>
+			<tbody id="body-table-venta">
+
+			</tbody>
+		</table>
+		<div class="row" id="div-total-venta">
+			<div class="col-xs-6 col-sm-6 col-md-8 col-lg-10 col-xl-10">
+				<b class="info-total-venta">I want your help please</b>
+			</div>
+			<div class="col-xs-6 col-sm-6 col-md-4 col-lg-2 col-xl-2">
+				<p>TOTAL</p>
+			</div>
+		</div>
 	</div>
 </div>
