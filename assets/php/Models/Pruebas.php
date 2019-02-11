0347c1455c820c4ee72f0ff86b2d69e3b249251d
Juanma <jpajoyl@unal.edu.co>
2019-02-11 16:05:26 -0500

    k

--- assets/php/Models/Pruebas.php
+++ assets/php/Models/Pruebas.php
@@ -13,6 +13,8 @@ include 'Venta.php';
 include 'TipoVenta.php';
 include '../Controllers/Response.php';
 include 'comprobanteEgreso.php';
+/*$ce = ComprobanteEgreso::obtenerComprobanteEgreso(1);
+$ce->imprimirComprobante(false);
 $ce = Factura::obtenerFactura(1);
-$ce->imprimirFacturaCarta();
+$ce->imprimirFacturaCarta();*/
  ?>
